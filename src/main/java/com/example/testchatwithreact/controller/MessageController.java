db1988bd69af564bc4f2fc1e9f0bd5ee6b54de2f
Philyaa3 <psychevskiy@gmail.com>
2023-04-24 20:41:47 +0300

    zxc

--- src/main/java/com/example/testchatwithreact/controller/MessageController.java
+++ src/main/java/com/example/testchatwithreact/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.example.testchatwithreact.controller;
 
 import com.example.testchatwithreact.model.MessageDTO;
+import com.example.testchatwithreact.service.StorageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,14 +12,21 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class MessageController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
+    private final StorageService storageService;
 
     @MessageMapping("/message")
     public ResponseEntity<Void> receivePublicMessages(@Payload MessageDTO message){
-        simpMessagingTemplate.convertAndSend("/chatroom/" + message.getRecipientName(), message);
+        if(message.getStatus().name().equals("JOIN"))
+            storageService.addUser(message.getSenderName());
+        else {
+            simpMessagingTemplate.convertAndSend("/chatroom/" + message.getRecipientName(), message);
+        }
+//        System.out.println(storageService.getAll());
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/testchatwithreact/controller/UserController.java
+++ src/main/java/com/example/testchatwithreact/controller/UserController.java
@@ -3,15 +3,13 @@ package com.example.testchatwithreact.controller;
 import com.example.testchatwithreact.service.StorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final StorageService storageService;
